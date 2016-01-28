7134df26ad711c34941b8610be11a7e2bd94f6b2
koehnden <dkoehn@hotmail.de>
2016-01-28 17:09:33 +0100

    Create LoadPackages.R

---
+++ LoadPackages.R
@@ -0,0 +1,10 @@
+##################### Packages for IS-Seminar #############################
+#
+############## load standard ML libraries
+library(caret)
+library(nnet)
+library(e1071)
+library(randomForest)
+
+############## load other packages 
+library(vegan)  # for random permutation of rows and columns
