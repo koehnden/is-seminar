7cd93647004a8623fde62d33abaae876eb540b64
koehnden <dkoehn@hotmail.de>
2016-02-09 22:49:18 +0100

    Update and rename IWFSfunction.R to IWFSfilter.R

--- IWFSfilter.R
+++ IWFSfilter.R
@@ -10,7 +10,7 @@
 # 
 #
 ###### Interaction Weight function
-# Calculates IW = 1 + I(F_i;F_j;C)/H(F_i) + H(F_i)
+# Calculates IW = 1 + I(F_i;F_j;C)/H(F_i) + H(F_j)
 # where a H(X) is the entropy function of X and I(X;Y;Z) is the interaction gain 
 # of three random variables X, Y and Z
 ## The function needs the following input
@@ -20,13 +20,25 @@
 # Note that all inputs must be discrete. So contiuos variables must be discretized first. 
 IW <- function(x,y,target){
   
-  ## Compute Interaction Gain
-  # I(F_i,F_j,C) = H(F_i) + H(F_j) + H(C) - H(F_i,F_j,C)  
-  IG <- interinformation(data.frame(x,y,target), method="emp")
+  # compute I(F_i;target;|F_j)
+  I_cond <- condinformation(X=x, Y=y, S=target, 
+                            method="emp")
+  # compute I(F_i;target)
+  I <- mutinformation(X=x, Y=target, method="emp")
+  
+  ## Compute Interaction Gain with I(F_i;F_j;C) = I(F_1;target;|F_2) - I(F_1;target)
+  IG <- I_cond - I
+  # Note that the author of the package infotheo defines the interaction gain exactly
+  # the other way: I(F_i,F_j;C) = I(F_1;target) -I(F_1;target;|F_2) 
+  # see my question as crossvalidated: 
+  # http://stats.stackexchange.com/questions/194192/computing-the-interaction-gain-is-there-an-error-in-the-infotheo-package-in-r
+  # and the paper Bontempi & Meyer (2010)
+  #IG <- -interinformation(data.frame(x,y,target), method="emp")
+  
   ## Compute Interaction weigth
   # IW(F_i,F_j) = 1 + I(F_i,F_j,C)/H(F_i) + H(F_j)
   # No braces around H(F_i) + H(F_j) in the paper, but then the IW is not beween 0 and 2
-  IW <- 1 + IG / (entropy(x, method="emp") +
+  IW <- 1 +  IG / (entropy(x, method="emp") +
                    entropy(y, method="emp"))
   return(IW)
 }
@@ -46,13 +58,11 @@ IW <- function(x,y,target){
 ## output: a vector of the feature names choosen by the algorithm or an index if 
 #          column names are not provided
 
-iwfs <- function(target, features, K=NA, colnames=TRUE,
-                 minfo.criteria=FALSE, percent.info=0.75){  
+iwfs <- function(target, features, K=NA, colnames=TRUE){  
   C <- target
-  allFeatures <- features
     
   ######### First Round
-  candidateFeatures <- allFeatures
+  candidateFeatures <- features
   # Initialize weights to 1 for each feature, w(F_i) in the paper
   weights <- c(rep(1,ncol(candidateFeatures)))
   
@@ -61,29 +71,33 @@ iwfs <- function(target, features, K=NA, colnames=TRUE,
   df <- data.frame(C,candidateFeatures)
   SU <- symmetrical.uncertainty(C~.,data=df); SUfull <- SU
   
+  # make sure that candidateFeature have the same names as the row in SU
+  # this caused the error "e[[j]] out of bounds" 
+  colnames(candidateFeatures) <- rownames(SU)
+  
   ### calulate the Relevance measure with all features for the first round
   adjR <- weights * (1 + SU) # weight adjusted Relevance Measure
   # in case no column names are provided
   if(colnames==FALSE){
-    rownames(adjR) <- NULL
+    #rownames(adjR) <- NULL
     choosenFeature <- cutoff.k(adjR,k=1) # choose the bset feature
     idx.new <- as.numeric(choosenFeature) # index of the choosen feature
-    } else {
-      choosenFeature <- cutoff.k(adjR,k=1) # choose the bset feature
-      idx.new <- which(names(allFeatures) %in% choosenFeature) # index of the choosen feature
+    } else {   
+      choosenFeature <- cutoff.k(adjR,k=1) # choose the best feature
+      idx.new <- which(names(candidateFeatures) %in% choosenFeature) # index of the choosen feature       
     }#end if else 
   
   ### exclude selected feature out of the candidate Features
   candidateFeatures <- candidateFeatures[,-idx.new]
+  
   ###### end First Round
   
   ### Round 2 to K
-  infoFull <- multiinformation(data.frame(features,target), method ="emp") 
+  #infoFull <- multiinformation(data.frame(features,target), method ="emp") 
   K <- K     
   k <- 1
   while(k < K){
     ### update weights
-    #print(ncol(candidateFeatures))
     update.weights <- c(rep(0,ncol(candidateFeatures))) 
     for(i in 1:ncol(candidateFeatures)){                  
       update.weights[i] <- IW(candidateFeatures[,i],
@@ -93,9 +107,9 @@ iwfs <- function(target, features, K=NA, colnames=TRUE,
     
     ### Calculate Relevance Measure
     if(colnames==FALSE){rownames(SU) <- NULL}
-    SU <-  subset(SU,rownames(SU) %in% colnames(candidateFeatures))   
+    SU <-  subset(SU,rownames(SU) %in% colnames(candidateFeatures))
     adjR <- weights * (1 + SU)
-
+    
     ### choose the the feature with the largest Relevance Measure (adjR)
     choosenNew <- cutoff.k(adjR,k=1)  
     choosenFeature <- append(choosenFeature,choosenNew, after=k)
@@ -103,14 +117,7 @@ iwfs <- function(target, features, K=NA, colnames=TRUE,
     
     ### exclude selected feature out of the candidate Features
     candidateFeatures <- candidateFeatures[,-idx.new]
-    ## if mutinformation as stopping criteria is defined calculate I(F_subset;C)
-    if(minfo.criteria==TRUE){
-      infoSubset <- multiinformation(data.frame(features[,choosenFeature],target),
-                             method ="emp")
-    }#end if  
-    if(minfo.criteria==TRUE & infoSubset >= infoFull*percent.info){
-      break
-    }
+  
     k <- k + 1
   }#end while
 
@@ -129,20 +136,25 @@ iwfs <- function(target, features, K=NA, colnames=TRUE,
 #       means that the maximum number of selected features is too high e.g.
 #       K >= # of features in the full set
 
-## test on the corral data
-#iwfs.corral <- iwfs(target=corral[,7],features=corral[,-7],
-#                  K=4, minfo.criteria=T, colnames=T)
+## test on the xor
+iwfs.xor <- iwfs(target=xor_100[,"target"],features=xor_100[,-98],
+                  K=10, colnames=T)
 ## get the feature subset
-iwfs.corral$selected.features 
-# note that iwfs works not well with corral and xor because the SU fails (univariate Filter)
-## corral: SU=0 for all features except 6
-## xor: SU=0 for all feature
+iwfs.xor$selected.features
+#iwfs.xor$symmetrical.uncertainty
+# SU=0 for all features --> IWFS looks for the Features that interact the most
+# with the first column (that is f1) than chooses f2 because it interacts the most
+# so somehow this is random. On the other hand, in reality there will probably
+# not a situation where SU=0 for all features as in the xor problem
 
 ## test on monk3 data
 #iwfs.monk <- iwfs(target=monk.tr[,"target"], features=monk.tr[,-c(1,8)],
-#                    K=5, minfo.criteria=T)
+#                    K=3)
+#iwfs.monk$selected.features
 
 ## test on madelon 
 # takes about 5min
 #iwfs.madelon <- iwfs(target=madelon[,"target"], features=madelon[,-501],
 #                    K=54, minfo.criteria=T, colnames=FALSE)
+
+
