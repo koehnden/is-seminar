fbd59fa20f041d4c5887ad63a604274cfb89572e
koehnden <dkoehn@hotmail.de>
2016-02-15 09:39:55 +0100

    Update LoadPackages.R

--- LoadPackages.R
+++ LoadPackages.R
@@ -6,9 +6,10 @@ library(nnet)
 library(e1071)
 library(randomForest)
 library(FSelector)
+library(microbenchmark)
 
-library(sideChannelAttack) # for mRMR
-library(Biocomb) # run install.packages("Biocomb", type="source")
+#library(sideChannelAttack) # for mRMR
+#library(Biocomb) # run install.packages("Biocomb", type="source")
 library(CORElearn)
 
 ############## load packages for Information Theory
@@ -16,5 +17,3 @@ library(infotheo)
 
 ############## load other packages 
 library(vegan)  # for random permutation of rows and columns
-
-
