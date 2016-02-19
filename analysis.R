1bc38494864901613f6fa6f1a454758f8131455a
koehnden <dkoehn@hotmail.de>
2016-02-19 11:24:16 +0100

    Update analysis.R
    function to compute auc and new non-linear data set

--- analysis.R
+++ analysis.R
@@ -3,33 +3,45 @@ source("LoadPackages.R")
 
 # load up the datasets and filter, performance functions
 source("applyFeatureSelection.R")
-source("LoadArtificialData.R")
-source("LoadRealData.R")
+#source("LoadArtificialData.R")
+#source("LoadRealData.R")
 source("performance.R")
 
-# create a list of all datasets we want to test
-datasets <- list(corral,corral_100,xor,xor_100,parity,toy,data1,toy.data2,toy.data3,monk,madelon)
-
-# Assumptions: Last variable of the dataset is the target variable
-
 #################### Analysis ##############################################
-resultsSelectedFeatures <- list()
-resultsPerformance <- list()
-
-# Loop selects the target variable, selects the features
-for (i in c(1:2)) {
-
-  d <- datasets[i]
-  selectedFeatures <- applyFS(d)
-  resultsSelectedFeatures[i] <- selectedFeatures
-  
-  # Performance
-  for (j in c(1:length(selectedFeatures))) {
-    targetVariable <- d[,ncol(c)]
-    d <- d[,1:ncol(d)-1]
-    
-    performance <- modelPerformance(d[,names(d) %in% selectedFeatures[j]],targetVariable)
-    resultsSelectedFeatures[j] <- performance
+# AUC Performance of each subset using SVM, NN, RF and GBM
+auc.subset <- function(data,selectedFeatures, level = c(0,1), linear=T){
+  targetVariable <- factor(data$target,levels=level,labels=c("yes","no"))
+  for (i in 1:length(selectedFeatures)){
+    df <- data[,names(data) %in% unlist(unlist(selectedFeatures[i]))]
+    df <- data.frame(df)
+    names(df) <- unlist(unlist(selectedFeatures[i]))
+    performance <- modelPerformance(df,targetVariable,linear=linear)
+    assign(paste("performance",i,sep=""),performance)
   }
-  
+  return(list(auc.relief=performance1,auc.consistency=performance2,auc.cfs=performance3,
+         auc.mRMR=performance4,auc.CMIM=performance5,auc.JMIM=performance6,
+         auc.IWFS=performance7,auc.FCFS=performance8,auc.ALL=performance9)) 
 }
+
+set.seed(123)
+corral.result <- auc.subset(data=corral,selectedFeatures=corral.fs)
+corral_100.result <- auc.subset(data=corral_100,selectedFeatures=corral_100.fs)
+
+xor.result <- auc.subset(data=xor_100,selectedFeatures=xor.fs,linear=F)
+# result for n=500: only Relief and IWFS identify both features
+# result for n=300: only Relief finds the both relevant features
+
+monk_noise.result <- auc.subset(data=monk.tr,selectedFeatures=monk_noise.fs)
+monk_test.result <- auc.subset(data=monk.test,selectedFeatures=monk_test.fs)
+parity.result <- auc.subset(data=parity,selectedFeatures=parity.fs,linear=F)
+
+# contiunos features with target={-1,1}
+toy1.result <- auc.subset(data=toy.data1,selectedFeatures=toy1.fs,level=c(-1,1))
+toy2.result <- auc.subset(data=toy.data2,selectedFeatures=toy2.fs,level=c(-1,1))
+toy3.result <- auc.subset(data=toy.data3,selectedFeatures=toy3.fs,level=c(-1,1))
+
+nonlinear.result <- auc.subset(data=toy1.nonlinear,
+                              selectedFeatures=nonlinear.fs,level=c(-1,1),linear=F)
+
+
+
